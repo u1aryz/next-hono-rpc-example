9a13ab6924adc0329eb70f76ce0352687082b0d7
u1aryz <u1aryz.d@gmail.com>
2023-12-05 21:46:36 +0900

    Add environment variable for API base URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { AppType } from "@/app/api/[[...route]]/route";
+import { env } from "@/env.mjs";
 import { hc } from "hono/client";
 
 export default async function Home() {
-	const client = hc<AppType>("http://localhost:3000");
+	const client = hc<AppType>(env.NEXT_PUBLIC_API_BASE_URL);
 	const res = await client.api.hello.$get({ query: { name: "Hono!" } });
 	const data = await res.json();
 	return <div>{data.message}</div>;
